c9aaef9e5fec5acfbff0a94f68ca73657b41c13e
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-12 18:26:09 -0500

    added formatting for survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -41,12 +41,42 @@ module.exports = function(app){
         let modalName = friends[allScores.indexOf(Math.min(...allScores))].name;
         let modalPictureURL = friends[allScores.indexOf(Math.min(...allScores))].picture;
         // @ need modal HERE before new person gets put into data
-        
+        // var html = `<!-- Modal -->
+        // <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        //   <div class="modal-dialog" role="document">
+        //     <div class="modal-content">
+        //       <div class="modal-header">
+        //         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        //         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        //           <span aria-hidden="true">&times;</span>
+        //         </button>
+        //       </div>
+        //       <div class="modal-body">
+        //         <p id="matchName">${modalName}</p>
+        //         <img alt="profilepic" id="matchPicture" src=${modalPictureURL}>
+        //       </div>
+        //       <div class="modal-footer">
+        //         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        //         <button type="button" class="btn btn-primary">Save changes</button>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
+        // <script src="text/javascript">
+        // $('#myModal').modal({
+        //     keyboard: false
+        //   })
+        // </script>`
+
+
+
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        // res.send(html);
         // @ eventually add logic to add new friend
         friends.push(newFriend);
         // console.log("this is friends.friends after a new one should have been added");
         // console.log(friends);
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        
     });
     // @ keeping this in for testing purposes
     app.post("/api/clear", function(req, res) {
